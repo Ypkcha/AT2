7fa9fc604e7b44acf34c2a112b026851d4ef1764
Yuri <yuripignotti2007@gmail.com.br>
2025-11-13 18:43:43 -0300

    commit de teste

---
+++ src/main/java/com/fatec/at2/AT2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fatec.at2;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class AT2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/fatec/at2/model.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fatec.at2;
+
+/**
+ *
+ * @author fatec-dsm2
+ */
+public class model {
+    
+}
